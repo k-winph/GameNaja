7dd8911629eca9d63619c2bb90897ab1cb6ddb5d
k-winph <win.kawinphop04@gmail.com>
2025-02-19 02:12:10 +0700

    feat(text): โชว์ข้อความทีละตัว

---
+++ project-game-jam/scene/cutscene/cutscene.gd
@@ -0,0 +1,38 @@
+extends Node2D
+	
+@onready var text = $text
+@onready var text_audio = $text_audio
+
+var full_text = "ระวังตัวด้วยนะหลานเอ้ย"
+var current_index = 0
+var typing_delay : float = 0.1
+var timer : float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	text.text = ""
+	text.visible = true
+	set_process(true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer += delta
+	if timer >= typing_delay and current_index < full_text.length():
+		text.text += full_text[current_index]  # เพิ่มตัวอักษร
+		current_index += 1  # เพิ่มดัชนี
+		timer = 0  # รีเซ็ตตัวจับเวลา
+
+func _start():
+	var full_text = text.text
+	text_audio.play()
+	text.text = ""
+	text.visible = true
+	await show_text(full_text)
+
+# ฟังก์ชันแสดงข้อความ
+func show_text(full_text: String) -> void:
+	var typing_speed = typing_delay / 10
+	for i in range(full_text.length()):
+		text.text += full_text[i]
+		await get_tree().create_timer(typing_speed).timeout
+	text_audio.stop()
