880ca65780ccdc79cc58c7f30e9245d613dbfbbb
k-winph <win.kawinphop04@gmail.com>
2025-02-19 00:53:21 +0700

    fix(player, done): แก้ปัญหาที่ตัวละครเคลื่อนที่ได้ก่อน cutscene จบ

--- project-game-jam/player/player.gd
+++ project-game-jam/player/player.gd
@@ -1,35 +1,57 @@
 extends CharacterBody2D
 
+@onready var player: CharacterBody2D = $"."
+@onready var path_follow_2d: PathFollow2D = $"../Path2D/PathFollow2D"
 
+var CanMove = false
+var ProgressRatio:float = 0.0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _process(delta):
+	if path_follow_2d != null:
+		ProgressRatio = path_follow_2d.ProgressRatio 
+		
+	wait_until_cutscene_complete()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	#movement
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	#animation
-	if velocity.x < 0:
-		get_node("AnimatedSprite2D").play("Run")
-		get_node("AnimatedSprite2D").set_flip_h(true)
-	elif velocity.x > 0:
-		get_node("AnimatedSprite2D").play("Run")
-		get_node("AnimatedSprite2D").set_flip_h(false)
-	else :
-		get_node("AnimatedSprite2D").play("Idle")
+	if CanMove == true:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		#animation
+		if velocity.x < 0:
+			get_node("AnimatedSprite2D").play("Run")
+			get_node("AnimatedSprite2D").set_flip_h(true)
+		elif velocity.x > 0:
+			get_node("AnimatedSprite2D").play("Run")
+			get_node("AnimatedSprite2D").set_flip_h(false)
+		else :
+			get_node("AnimatedSprite2D").play("Idle")
 
-	move_and_slide()
+		move_and_slide()
 	
+func wait_until_cutscene_complete():
+	if ProgressRatio >0.99:
+		CanMove = true
+		player.visible = true
+	else:
+		CanMove = false
+		player.visible = false
+	
+
+func wait_until_progress_complete():
+	while ProgressRatio <= 1.0:
+		await get_tree().process_frame
--- project-game-jam/scene/scene1/path_follow_2d.gd
+++ project-game-jam/scene/scene1/path_follow_2d.gd
@@ -1,26 +1,22 @@
 extends PathFollow2D
 
-@onready var player: CharacterBody2D = $"../../Player"
 @onready var path_2d: Path2D = $".."
 
+var ProgressRatio = 0.0
 var speed = 0.5
 # Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
 #	pass # Replace with function body.
-
+func setProgressRatio():
+	while progress_ratio <= 1.0:
+		ProgressRatio = progress_ratio
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	progress_ratio += delta * speed
 	if progress_ratio < 1:
+		ProgressRatio = progress_ratio
 		get_node("AnimatedSprite2D").play("Run")
 		get_node("AnimatedSprite2D").set_flip_h(false)
-		player.visible = false
 	elif progress_ratio == 1 :
-		player.visible = true
-		await wait_until_progress_complete()
 		path_2d.visible = false
 		
-
-func wait_until_progress_complete():
-	while progress_ratio < 1.0:  # ตรวจสอบค่า progress_ratio
-		await get_tree().process_frame  # รอเฟรมถัดไป (ป้องกันเกมค้าง)
